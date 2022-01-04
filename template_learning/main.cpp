f42f8595ea6685de1a89cab96fbf4a95fd309c63
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-01-04 11:34:26 +0300

    Initial Commit

---
+++ template_learning/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  template_learning
+//
+//  Created by Jedidiah Uchenna on 04.01.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
